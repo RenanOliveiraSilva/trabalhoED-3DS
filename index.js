667868bc39bb48348cb4023e37c7b606c292c1f2
RenanOliveiraSilva <renandeoliveirasilva123@gmail.com>
2024-04-02 19:42:16 -0300

    V1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//teste commit Lucas1 - Computador
+//Função para entrada de dados de alunos
+function cadastraUser() {
+
+    let alunos = [];
+
+    let nome = prompt("Digite o nome do aluno: ");
+    let ra = prompt("Digite o RA desse aluno: ");
+    let idade = prompt("Digite a idade desse aluno: ");
+    let sexo = prompt("Digite o sexo desse aluno: ");
+    let media = prompt("Digite a média desse aluno: ");
+    let resultado = prompt("Digite se o aluno é aprovado ou reprovado: ");
+
+    let alunoObj = {
+        nome,
+        ra,
+        idade,
+        sexo,
+        media,
+        resultado
+
+    }
+
+
+    for (const value in alunoObj) {
+        
+    }
+    alunos = [ ...alunoObj ];
+
+    console.log(alunos);
+
+    }
+
+    cadastraUser();
